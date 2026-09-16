d33cee6fd8be3c4bd6075dc2d4efe931169097fc
Matt <mvass2@nait.ca>
2026-09-16 08:23:06 -0600

    Elif number picker ez

---
+++ elif.py
@@ -0,0 +1,7 @@
+number = float(input("Enter a number between 1 and 100: "))
+if number > 50:
+    print("Your number is larger than 50")
+elif number == 50:
+    print("Your number is 50")
+else:
+    print("Your Number is smaller than 50")
