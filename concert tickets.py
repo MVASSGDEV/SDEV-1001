108edd3e6ebea991c019e3eb3fafb71a8b6a9812
Matt <mvass2@nait.ca>
2026-09-16 10:41:34 -0600

    Concert Tickets added

---
+++ concert tickets.py
@@ -0,0 +1,13 @@
+concert_name = input("What Concert are you going to?: ")
+
+if concert_name == "sabaton" or concert_name == "Sabaton":
+    print("You are in the right place")
+    has_ticket = input("Do you have your ticket?")
+
+
+
+elif concert_name == "Chapell Roan":
+    print("get out.")
+else:
+    print("Wrong Venue Buddy")
+
